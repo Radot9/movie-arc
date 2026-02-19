e97a58b5ca5b7a69cee90750820d80b1d75e76db
Emmanuel Akintunde <nerad9@gmail.com>
2026-02-19 10:35:21 +0100

    Add deploy workflow and correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Use repo name casing to match GitHub Pages path.
-  base: '/MovieArc/',
+  // Use repo name (lowercase) to match the new GitHub Pages path.
+  base: '/movie-arc/',
   plugins: [react()],
 })
